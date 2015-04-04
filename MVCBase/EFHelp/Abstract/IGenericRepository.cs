2f43c2ffc59a92b5a1e11910ea49e8780e3142ad
Lebda <lebduska.jiri@seznam.cz>
2015-04-04 14:14:00 +0200

    Start comit

---
+++ MVCBase/EFHelp/Abstract/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace EFHelp.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> SelectAll();
+        T SelectByID(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
---
+++ MVCBase/EFHelp/Concrete/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using EFHelp.Abstract;
+
+namespace EFHelp.Concrete
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private DbContext db = null;
+        private DbSet<T> table = null;
+
+        public GenericRepository(DbContext db)
+        {
+            this.db = db;
+            table = db.Set<T>();
+        }
+
+        public IEnumerable<T> SelectAll()
+        {
+            return table.ToList();
+        }
+
+        public T SelectByID(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            db.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
