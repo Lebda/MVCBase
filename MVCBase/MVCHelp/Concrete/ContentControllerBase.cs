65de220273f1632848a5a1cc275c53e93cd397d3
Lebda <lebduska.jiri@seznam.cz>
2015-05-10 13:53:42 +0200

    player

---
+++ MVCBase/MVCHelp/Concrete/ContentControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCHelp.Concrete
+{
+    public class ContentControllerBase : Controller
+    {
+        public FileResult GetDeveloperLogoBase()
+        {
+            var path = Path.Combine("~/Content/Images", DeveloperLogo);
+            return File(path, "image/png");
+        }
+
+        protected string DeveloperLogo { get; set; }
+    }
+}
